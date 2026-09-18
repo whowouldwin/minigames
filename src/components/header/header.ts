12ecfc2258953eddff74ce8de24d44885243fac8
whowouldwin <leranetwork@gmail.com>
2026-09-19 00:03:05 +0200

    feat(RSS-QS-1-4-1): add header buttons and refine layout

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,4 +1,5 @@
 import { createSiteLogo } from '../ui/site-logo';
+import { createButton } from '../ui/button';
 import { createMainNavigation } from '../navigation/main-navigation';
 
 import './header.scss';
@@ -9,9 +10,27 @@ export const createHeader = (): HTMLElement => {
     header.className = 'header';
 
     const actions: HTMLDivElement = document.createElement('div');
-
     actions.className = 'header__actions';
-    actions.append(createMainNavigation());
+
+    const buttons: HTMLDivElement = document.createElement('div');
+    buttons.className = 'header__buttons';
+
+    const loginButton: HTMLButtonElement = createButton(
+        'Log In',
+        'outlined',
+    );
+
+    const signupButton: HTMLButtonElement = createButton(
+        'Sign Up',
+        'filled',
+    );
+
+    loginButton.classList.add('header__login-button');
+    signupButton.classList.add('header__signup-button');
+
+    buttons.append(loginButton, signupButton);
+
+    actions.append(createMainNavigation(), buttons);
 
     header.append(createSiteLogo(), actions);
 
